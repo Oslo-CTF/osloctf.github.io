b7350da00eb31ec25e28312ed4d160b79485e80c
Marie Ihle Frogner <marieihlefrogner@gmail.com>
2021-08-03 13:33:30 +0200

    add schedule

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <Header />
     <main>
       <div class="container">
-        <!-- <Events/> -->
+        <Events/>
         <!-- <Team/> -->
       </div>
     </main>
@@ -12,12 +12,14 @@
 </template>
 
 <script>
-import Header from "./components/Header.vue";
+import Header from "./components/Header";
+import Events from "./components/Events";
 
 export default {
   name: "App",
   components: {
     Header,
+    Events,
   },
 };
 </script>

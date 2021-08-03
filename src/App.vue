ae0564fc89dfb7d50b4e097c559e92ea4d094f3b
Marie Ihle Frogner <marieihlefrogner@gmail.com>
2021-08-03 12:16:09 +0200

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
-  <Header msg="Oslo-CTF"/>
+  <div>
+    <Header />
+    <main>
+      <div class="container">
+        <!-- <Events/> -->
+        <!-- <Team/> -->
+      </div>
+    </main>
+    <!-- <Footer/> -->
+  </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
+import Header from "./components/Header.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Header
-  }
-}
+    Header,
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./styles/main.css"></style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,34 +1,60 @@
 <template>
-  <img alt="Logo" src="../assets/logo.svg" height="500">
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    
+  <div class="header">
+    <div class="headerContainer">
+      <img class="logo" alt="Logo" src="../assets/logo.svg" />
+      <div>
+        <h1 class="title">Oslo-CTF</h1>
+        <p class="description">
+          Oslo-CTF is a group of security enthusiasts and programmers. Based at
+          the University of Oslo and Oslo Metropolitan University, and now
+          scaling beyond to the Oslo area. We host ethical hacking workshops to
+          raise awareness of the importance of IT security. The workshops are
+          based on tasks from hacking competitions (CTFs), and show how to
+          exploit vulnerabilities. We regularly participate in CTFs, and meet up
+          to solve tasks together a few times each semester. You are more than
+          welcome to join us while solving hacking tasks! Join our discord
+          channel to get more information about joining us in hacking
+          competitions:
+          <a href="https://discord.gg/7aTwpGa">https://discord.gg/7aTwpGa</a>
+        </p>
+        <div class="links">
+          <div class="internalLinks">
+            <a class="internalLink" href="#schedule">View Schedule</a>
+            <a class="internalLink" href="#team">The Team</a>
+          </div>
+          <div class="externalLinks">
+            <a target="_blank" rel="noopener" class="iconButton" href="https://github.com/Oslo-CTF">
+              <img src="../assets/github.svg" alt="github" />
+            </a>
+            <a target="_blank" rel="noopener" class="iconButton" href="https://facebook.com/osloctf/">
+              <img src="../assets/facebook.svg" alt="facebook" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <svg class="trim" viewBox="0 0 1920 259">
+      <g transform="matrix(1,0,0,1,0,-821.452)">
+        <path
+          d="M0,948.736C163.974,898.948 379.499,770.145 557.751,870.149C932.766,1080.54 843.353,1061.82 1192.94,907.165C1296.43,861.381 1546.45,1036.3 1642.23,899.204C1775.75,708.075 1849.15,929.062 1920,932.307L1920,1080L0,1080L0,948.736Z"
+        />
+      </g>
+    </svg>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: 'Header',
+  name: "Header",
   props: {
-    msg: String
+
+  },
+  components: {
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped src="../styles/header.css"></style>
+<style scoped href="../styles/iconLink.css"></style>
